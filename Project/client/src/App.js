3acf33fea3118f800abacbeb6445facc2f54c567
Oscar <oscar.restrepo2@gmail.com>
2023-03-11 23:31:04 -0500

    feat: creacion flujo react-redux para mostrar listas de un usuario

--- Project/client/src/App.js
+++ Project/client/src/App.js
@@ -1,18 +1,13 @@
 import React from 'react';
-import TaskList from './TaskList';
+import TaskList from './components/TaskList';
 
 const App = () => {
-  return (
-    <div>
-      <h1>Listado de tareas</h1>
-      <TaskList />
-    </div>
-  );
+    return (
+        <div>
+            <h1>Listado de tareas</h1>
+            <TaskList />
+        </div>
+    );
 };
 
 export default App;
-
-
-
-
-
--- Project/client/src/redux/actions/actions.js
+++ Project/client/src/redux/actions/actions.js
@@ -1,10 +1,12 @@
 import axios from 'axios';
 
 export const fetchTasks = () => async (dispatch) => {
-  try {
-    const { data } = await axios.get('/tasks'); // Endpoint del backend para obtener las tareas
-    dispatch({ type: 'FETCH_TASKS_SUCCESS', payload: data }); // Actualizamos el estado de Redux con las tareas obtenidas
-  } catch (error) {
-    console.log(error);
-  }
+    try {
+        const { data } = await axios.get(
+            'http://localhost:4001/api/auth/640d210a783bc3ee5b6614a9/todos'
+        ); // Endpoint del backend para obtener las tareas
+        dispatch({ type: 'FETCH_TASKS_SUCCESS', payload: data }); // Actualizamos el estado de Redux con las tareas obtenidas
+    } catch (error) {
+        console.log(error);
+    }
 };
