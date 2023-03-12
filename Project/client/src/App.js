7605dd727d39fc24e20a8484e02d60a4d9710ff0
Oscar <oscar.restrepo2@gmail.com>
2023-03-11 23:05:00 -0500

    feat: front funcionando

---
+++ Project/client/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import TaskList from './TaskList';
+
+const App = () => {
+  return (
+    <div>
+      <h1>Listado de tareas</h1>
+      <TaskList />
+    </div>
+  );
+};
+
+export default App;
+
+
+
+
+
